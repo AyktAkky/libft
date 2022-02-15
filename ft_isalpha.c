5d4e3e0274d85a6e5568f8969c40abbc3e630954
Aykut Akkaya <aakkaya@k2m03s03.42kocaeli.com.tr>
2022-02-15 14:56:15 +0300

    ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,22 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aakkaya <aakkaya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/15 14:23:56 by aakkaya           #+#    #+#             */
-/*   Updated: 2022/02/15 14:45:52 by aakkaya          ###   ########.fr       */
+/*   Created: 2022/02/15 14:38:56 by aakkaya           #+#    #+#             */
+/*   Updated: 2022/02/15 14:54:00 by aakkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
+//eger karakter alfabede yer alıyorsa herhangi bir sayı değilse sıfır çevirir.
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
+int	ft_isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	return (0);
+}
+/*
+int	main(void)
+{
+	char c;
 
-int	ft_isalpha(int c);
-#endif
+	c = '2';
+	printf("%d", ft_isalpha(c));
+}*/
