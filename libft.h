720e3aab1bfdafb7d5ec7ecac67971ab0a326bd6
Aykut Akkaya <aakkaya@k2m03s03.42kocaeli.com.tr>
2022-03-06 00:47:59 +0300

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aakkaya <aakkaya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 14:23:56 by aakkaya           #+#    #+#             */
-/*   Updated: 2022/02/27 14:59:52 by aakkaya          ###   ########.fr       */
+/*   Updated: 2022/03/06 00:47:05 by aakkaya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,10 @@ int     ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_bzero(void *s, size_t n);
